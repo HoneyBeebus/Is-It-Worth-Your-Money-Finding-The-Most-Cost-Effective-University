782d13998056ffb62aee8eaf1e0446bba89917ea
Henry White (Macbook) <cwhite24@ucmerced.edu>
2019-10-23 12:06:24 -0700

    Initial Commit

---
+++ Project.R
@@ -0,0 +1,14 @@
+# Graphing Studen Faculty ratio and Graduation Rate #
+
+######################
+#Read in the database#
+######################
+college = read.csv(file="College.csv", header=TRUE,sep=",")
+
+####################
+#Create Data Frames#
+####################
+
+
+
+#cor(college)
